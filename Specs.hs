f524413831293f4f67364e1df7fcb865e056ad50
ToF- <cthibaut@octo.com>
2019-03-06 18:42:58 +0100

    next to center

--- Specs.hs
+++ Specs.hs
@@ -11,6 +11,14 @@ main = hspec $ do
                 it "of size 26x20" $ do
                     let t = theater ('A','Z') (1,20) 
                     offer t 1 `shouldBe` [('N',1)]
+
+            describe "after booking of one seat" $ do
+                it "in size 5x3" $ do
+                    let t = theater ('A','E') (1,3)
+                        o = offer t 1
+                        t'= book t o
+                    offer t' 1 `shouldBe` [('D',1)]
+
                     
             
                 
--- Theater.hs
+++ Theater.hs
@@ -1,6 +1,7 @@
 module Theater
 where
 import Data.Set as S
+import Data.List as L
 
 type Seat = (Char,Int)
 data Theater = Theater (Char,Char) (Int,Int) (Set Seat) 
@@ -12,7 +13,12 @@ theater rows cols = Theater rows cols
     (S.fromList [(row,col)| row <- range rows, col <- range cols])
 
 offer :: Theater -> Int -> [Seat]
-offer (Theater (min,max) _ _) _ = [(seat,1)]
+offer (Theater (min,max) _ set) _ = case S.member (seat,1) set of
+    True -> [(seat,1)]
+    False -> [(succ seat,1)]
     where r = range (min,max)
           seat = r!!middle
           middle = length r `div` 2
+
+book :: Theater -> [Seat] -> Theater
+book (Theater rows cols set) seats = Theater rows cols (L.foldl (flip S.delete )set seats)
